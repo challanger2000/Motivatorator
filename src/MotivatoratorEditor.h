c704bef2982a28bbebfc8309d611a33f51bf20c5
challanger2000 <challanger2000@gmx.de>
2026-09-02 00:51:40 +0200

    Fix custom editor inheritance

--- src/MotivatoratorEditor.h
+++ src/MotivatoratorEditor.h
@@ -1,24 +1,24 @@
 #pragma once
 
 #include "vstgui/plugin-bindings/vst3editor.h"
-#include "vstgui/uidescription/icontroller.h"
 #include "vstgui/lib/cviewcontainer.h"
 
 namespace Steinberg::Vst {
 
-class MotivatoratorEditor final : public VSTGUI::VST3Editor, public VSTGUI::IController {
+class MotivatoratorEditor final : public VSTGUI::VST3Editor {
 public:
     explicit MotivatoratorEditor(EditController* controller);
 
+protected:
     VSTGUI::CView* verifyView(VSTGUI::CView* view, const VSTGUI::UIAttributes& attributes,
                              const VSTGUI::IUIDescription* description) override;
-    VSTGUI::IControlListener* getControlListener(VSTGUI::UTF8StringPtr name) override;
     void valueChanged(VSTGUI::CControl* control) override;
 
 private:
     void showOptions(bool show);
     VSTGUI::CViewContainer* optionsPanel_ {nullptr};
     bool optionsVisible_ {false};
+    bool optionsButtonDown_ {false};
 };
 
 } // namespace Steinberg::Vst
