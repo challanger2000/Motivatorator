7eae69f074a8acb80925029061b21694112c8f92
challanger2000 <challanger2000@gmx.de>
2026-09-01 23:08:56 +0200

    Add phrase engine and character parameters

--- src/MotivatoratorProcessor.h
+++ src/MotivatoratorProcessor.h
@@ -2,6 +2,7 @@
 
 #include "public.sdk/source/vst/vstaudioeffect.h"
 #include "public.sdk/source/vst/vsteditcontroller.h"
+#include <cstdint>
 
 namespace Steinberg::Vst {
 
@@ -14,16 +15,46 @@ enum MotivatoratorParams : ParamID {
     kMuteId = 102,
     kOptionsId = 103,
     kLanguageId = 104,
-    kIntervalId = 105
+    kIntervalId = 105,
+    kCharacterId = 106,
+    kPhraseId = 107,
+    kPhraseToneId = 108
 };
 
 class MotivatoratorProcessor final : public AudioEffect {
 public:
     MotivatoratorProcessor();
     tresult PLUGIN_API initialize(FUnknown* context) SMTG_OVERRIDE;
+    tresult PLUGIN_API setupProcessing(ProcessSetup& setup) SMTG_OVERRIDE;
     tresult PLUGIN_API process(ProcessData& data) SMTG_OVERRIDE;
     tresult PLUGIN_API canProcessSampleSize(int32 symbolicSampleSize) SMTG_OVERRIDE;
     static FUnknown* createInstance(void*) { return static_cast<IAudioProcessor*>(new MotivatoratorProcessor()); }
+
+private:
+    void handleParameters(ProcessData& data);
+    void chooseNextPhrase();
+    void emitPhrase(ProcessData& data);
+    void resetIntervalCounter();
+    int nextDeckIndex(bool motivator);
+
+    double sampleRate_ {44100.0};
+    int mode_ {0};
+    int language_ {0};
+    int interval_ {2};
+    bool muted_ {false};
+    bool nextState_ {false};
+    bool mixedNextPositive_ {true};
+    bool phrasePositive_ {true};
+    bool needsPhraseEmit_ {true};
+    int motivatorPos_ {0};
+    int demotivatorPos_ {0};
+    int motivatorStart_ {0};
+    int demotivatorStart_ {9};
+    int motivatorStep_ {5};
+    int demotivatorStep_ {7};
+    int currentPhraseGlobal_ {0};
+    int64 samplesUntilNext_ {0};
+    uint32_t intervalRng_ {0x4D4F5449u};
 };
 
 class MotivatoratorController final : public EditControllerEx1 {
