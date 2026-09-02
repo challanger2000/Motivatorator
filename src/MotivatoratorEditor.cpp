abd0b45fdc621289b923e7d820c44da20406da12
challanger2000 <challanger2000@gmx.de>
2026-09-02 04:10:22 +0200

    Add dynamic GNOMI character switching

--- src/MotivatoratorEditor.cpp
+++ src/MotivatoratorEditor.cpp
@@ -1,8 +1,74 @@
 #include "MotivatoratorEditor.h"
+#include "MotivatoratorProcessor.h"
+#include "vstgui/lib/cbitmap.h"
+#include "vstgui/lib/cvstguitimer.h"
+#include "vstgui/uidescription/uiattributes.h"
+#include <algorithm>
 
 namespace Steinberg::Vst {
 
+namespace {
+
+class CharacterView final : public VSTGUI::CView {
+public:
+    CharacterView(const VSTGUI::CRect& size, EditController* controller)
+    : CView(size), controller_(controller) {
+        positive_ = VSTGUI::makeOwned<VSTGUI::CBitmap>(VSTGUI::CResourceDescription("gnomi_positive.png"));
+        negative_ = VSTGUI::makeOwned<VSTGUI::CBitmap>(VSTGUI::CResourceDescription("gnomi_negative.png"));
+
+        if (positive_ && positive_->getPlatformBitmap())
+            positive_->getPlatformBitmap()->setScaleFactor(4.0);
+        if (negative_ && negative_->getPlatformBitmap())
+            negative_->getPlatformBitmap()->setScaleFactor(4.0);
+
+        timer_ = VSTGUI::makeOwned<VSTGUI::CVSTGUITimer>(
+            [this](VSTGUI::CVSTGUITimer*) { invalid(); }, 100);
+    }
+
+    void draw(VSTGUI::CDrawContext* context) override {
+        if (!controller_) {
+            setDirty(false);
+            return;
+        }
+
+        const auto modeValue = controller_->getParamNormalized(kModeId);
+        const auto toneValue = controller_->getParamNormalized(kPhraseToneId);
+        const int mode = std::clamp(static_cast<int>(modeValue * 3.0), 0, 2);
+
+        bool useNegative = false;
+        if (mode == 1)
+            useNegative = true;
+        else if (mode == 2)
+            useNegative = toneValue >= 0.5;
+
+        auto bitmap = useNegative ? negative_ : positive_;
+        if (bitmap) {
+            const auto r = getViewSize();
+            VSTGUI::CRect dest(0., 0., r.getWidth(), r.getHeight());
+            bitmap->draw(context, dest, VSTGUI::CPoint(0., 0.), 1.f);
+        }
+        setDirty(false);
+    }
+
+private:
+    EditController* controller_ {nullptr};
+    VSTGUI::SharedPointer<VSTGUI::CBitmap> positive_;
+    VSTGUI::SharedPointer<VSTGUI::CBitmap> negative_;
+    VSTGUI::SharedPointer<VSTGUI::CVSTGUITimer> timer_;
+};
+
+} // namespace
+
 MotivatoratorEditor::MotivatoratorEditor(EditController* controller)
-: VST3Editor(controller, "view", "Motivatorator.uidesc") {}
+: VST3Editor(controller, "view", "Motivatorator.uidesc"), controller_(controller) {}
+
+VSTGUI::CView* MotivatoratorEditor::createView(const VSTGUI::UIAttributes& attributes,
+                                                const VSTGUI::IUIDescription* description) {
+    if (const auto name = attributes.getAttributeValue(VSTGUI::IUIDescription::kCustomViewName)) {
+        if (*name == "CharacterView")
+            return new CharacterView(VSTGUI::CRect(0., 0., 390., 300.), controller_);
+    }
+    return VSTGUI::VST3Editor::createView(attributes, description);
+}
 
 } // namespace Steinberg::Vst
