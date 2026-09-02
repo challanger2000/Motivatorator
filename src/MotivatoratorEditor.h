8ae07607aa701f120c3493b89d84d139c119a696
challanger2000 <challanger2000@gmx.de>
2026-09-02 02:41:29 +0200

    Simplify editor to stock VST3Editor

--- src/MotivatoratorEditor.h
+++ src/MotivatoratorEditor.h
@@ -1,25 +1,12 @@
 #pragma once
 
 #include "vstgui/plugin-bindings/vst3editor.h"
-#include "vstgui/lib/cviewcontainer.h"
 
 namespace Steinberg::Vst {
 
 class MotivatoratorEditor final : public VSTGUI::VST3Editor {
 public:
     explicit MotivatoratorEditor(EditController* controller);
-
-protected:
-    VSTGUI::CView* verifyView(VSTGUI::CView* view, const VSTGUI::UIAttributes& attributes,
-                             const VSTGUI::IUIDescription* description) override;
-    void valueChanged(VSTGUI::CControl* control) override;
-
-private:
-    void showOptions(bool show);
-
-    VSTGUI::CViewContainer* optionsPanel_ {nullptr};
-    bool optionsVisible_ {false};
-    bool optionsButtonDown_ {false};
 };
 
 } // namespace Steinberg::Vst
