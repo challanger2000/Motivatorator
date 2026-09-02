ee40d53d62c3aac3de45ffc16f4441c9e056484b
challanger2000 <challanger2000@gmx.de>
2026-09-02 04:10:01 +0200

    Add custom character view hook

--- src/MotivatoratorEditor.h
+++ src/MotivatoratorEditor.h
@@ -7,6 +7,13 @@ namespace Steinberg::Vst {
 class MotivatoratorEditor final : public VSTGUI::VST3Editor {
 public:
     explicit MotivatoratorEditor(EditController* controller);
+
+protected:
+    VSTGUI::CView* createView(const VSTGUI::UIAttributes& attributes,
+                              const VSTGUI::IUIDescription* description) override;
+
+private:
+    EditController* controller_ {nullptr};
 };
 
 } // namespace Steinberg::Vst
