af5e2909e563073813422837e21e745fbbc0c4ed
challanger2000 <challanger2000@gmx.de>
2026-09-03 12:09:34 +0200

    Expose demo as separate plugin

--- src/MotivatoratorFactory.cpp
+++ src/MotivatoratorFactory.cpp
@@ -4,6 +4,29 @@
 using namespace Steinberg;
 using namespace Steinberg::Vst;
 
+#ifdef MOTIVATOR_DEMO
+BEGIN_FACTORY_DEF("Motivator Demo", "https://github.com/challanger2000/", "")
+
+DEF_CLASS2(INLINE_UID_FROM_FUID(MotivatoratorProcessorUID),
+           PClassInfo::kManyInstances,
+           kVstAudioEffectClass,
+           "Motivator Demo",
+           Vst::kDistributable,
+           Vst::PlugType::kFx,
+           "0.1.0-demo",
+           kVstVersionString,
+           MotivatoratorProcessor::createInstance)
+
+DEF_CLASS2(INLINE_UID_FROM_FUID(MotivatoratorControllerUID),
+           PClassInfo::kManyInstances,
+           kVstComponentControllerClass,
+           "MotivatorDemoController",
+           0,
+           "",
+           "0.1.0-demo",
+           kVstVersionString,
+           MotivatoratorController::createInstance)
+#else
 BEGIN_FACTORY_DEF("Motivator", "https://github.com/challanger2000/", "")
 
 DEF_CLASS2(INLINE_UID_FROM_FUID(MotivatoratorProcessorUID),
@@ -25,5 +48,6 @@ DEF_CLASS2(INLINE_UID_FROM_FUID(MotivatoratorControllerUID),
            "0.1.0",
            kVstVersionString,
            MotivatoratorController::createInstance)
+#endif
 
 END_FACTORY
