c120dc9a44cc1893bc7825c111e1b14c24bb56a2
challanger2000 <challanger2000@gmx.de>
2026-09-03 12:02:33 +0200

    Expose plugin as Motivator

--- src/MotivatoratorFactory.cpp
+++ src/MotivatoratorFactory.cpp
@@ -4,12 +4,12 @@
 using namespace Steinberg;
 using namespace Steinberg::Vst;
 
-BEGIN_FACTORY_DEF("Motivatorator", "https://github.com/challanger2000/", "")
+BEGIN_FACTORY_DEF("Motivator", "https://github.com/challanger2000/", "")
 
 DEF_CLASS2(INLINE_UID_FROM_FUID(MotivatoratorProcessorUID),
            PClassInfo::kManyInstances,
            kVstAudioEffectClass,
-           "Motivatorator",
+           "Motivator",
            Vst::kDistributable,
            Vst::PlugType::kFx,
            "0.1.0",
@@ -19,7 +19,7 @@ DEF_CLASS2(INLINE_UID_FROM_FUID(MotivatoratorProcessorUID),
 DEF_CLASS2(INLINE_UID_FROM_FUID(MotivatoratorControllerUID),
            PClassInfo::kManyInstances,
            kVstComponentControllerClass,
-           "MotivatoratorController",
+           "MotivatorController",
            0,
            "",
            "0.1.0",
